89ad0cffd88b508673da497bf6af90448a21baf9
zaba37 <zaba37@gmail.com>
2018-09-19 22:55:32 +0200

    Fixes

---
+++ hardspace.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ "$1" ]; then
+        cat "$1" | sed '
+        / [aeiouwzAEIOUWZ] *$/ {
+        N
+        s/  *\([aeiouwzAEIOUWZ]\)\n */ \1~/g
+        }' | sed 's/^ *\([aeiouwzAEIOUWZ]\)  */ \1~/g'  | sed 's/  *\([aeiouwzAEIOUWZ]\)  */ \1~/g'
+else
+        echo przyklad uzycia - nazwa_skryptu nazwa_pliku 
+fi
+#!/bin/bash
+if [ "$1" ]; then
+        cat "$1" | sed '
+        / [aeiouwzAEIOUWZ] *$/ {
+        N
+        s/  *\([aeiouwzAEIOUWZ]\)\n */ \1~/g
+        }' | sed 's/^ *\([aeiouwzAEIOUWZ]\)  */ \1~/g'  | sed 's/  *\([aeiouwzAEIOUWZ]\)  */ \1~/g'
+else
+        echo przyklad uzycia - nazwa_skryptu nazwa_pliku 
+fi
